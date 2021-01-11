8e5edb6ff76334b934e7d35c151ea8db635d489c
Matthew Alexander <matt.bryantalex@gmail.com>
2021-01-11 15:11:13 -0800

    first commit

---
+++ RobotsVsDinosaurs/Battlefield.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsvsDinosaurs
+{
+    class Battlefield
+       
+    {
+        //Chooose a Robot from the list of Robots
+        
+        //and Choose a Dinosaur from the list
+
+        public void RunBattle()
+        {
+
+        }
+
+        //Have Robot attack the dinosaur (take a turn)
+
+        //Reduce the health of the dinosaur by the AttackRobot method (robotAttackPower * robotPowerLevel)
+
+        //Reduce the health of the robot by -10
+
+        //Print out "The robot hit the dinosaur and reduces the dinosaur's health by " ". The dinosaur now has " " health. The robot has its attack power reduced by " ".
+
+
+        //If the robots health is > 0, continue the fight
+
+        //If the robots health is < = 0, print out "The Dinosaur has won!" and end the fight
+
+        //Have Dinosaur attack the robot (take a turn)
+
+        //Reduce the dinosaurs health based on the attack power of the robot
+
+        //If the dinosaurs health is  > 0, continue the fight
+
+        //If the dinosaur's health is <= 0, print "The Robot has won!" and end the fight
+       
+        //Idea: create a loop and decrement the health of each robot and dinosaur after turn
+    }
+}
---
+++ RobotsVsDinosaurs/Fleet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsvsDinosaurs
+{
+    class Fleet
+    {
+        //robot created objects stored here
+
+    }
+}
